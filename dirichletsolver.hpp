afd99a3508cf4c05b7da810a6a8d33f82dea9b76
rclwxc <rclwxc@mst.edu>
2018-05-02 23:18:51 -0500

    added createMatrix function

--- dirichletsolver.hpp
+++ dirichletsolver.hpp
@@ -16,7 +16,7 @@ DirichletSolver<T_returnType,T_functionPtr>::DirichletSolver(const int& n, const
 {
   m_numDivisions=n;
   m_func=f;
-  //createMatrix();
+  createMatrix();
   createVector();
 }
 
@@ -32,11 +32,50 @@ MyVector<T_returnType> DirichletSolver<T_returnType,T_functionPtr>::computeSteep
   return SteepestDescent<T_returnType>()(m_matrix,m_vector);
 }
 
+
 template <class T_returnType, class T_functionPtr>
 void DirichletSolver<T_returnType,T_functionPtr>::createMatrix()
 {
-  m_matrix.setSize(pow(m_numDivisions-1,2));
-  //todo
+  int matrix_size = (m_numDivisions-1) * (m_numDivisions-1);
+  SymmetricMatrix<double> m_matrix(matrix_size);
+  
+  double h = 1.0/m_numDivisions;
+
+  double x_vals = 0;
+  double y_vals = 0;
+
+  bool r, t;
+
+
+  for (int i=0; i<matrix_size; i++)
+  {
+    x_vals += h;
+
+    if (i%(m_numDivisions-1) == 0)
+    {
+      y_vals += h;
+      x_vals = h;
+    }
+    
+    r = (m_numDivisions-2 != i%(m_numDivisions-1));
+    t = (i != matrix_size - 1);
+
+    for (int j=r; j<matrix_size; j++)
+    {
+      if (j == (i+(m_numDivisions-1)) && t)
+        m_matrix(i,j) = -h;
+      else if (j == (i+1) && r)
+        m_matrix(i,j) = -h;
+      else if (i == j)
+        m_matrix(i,j) = 1;
+      else
+        m_matrix(i,j) = 0;
+    }
+
+  }
+  //cout << "made it to end of createMatrix();" << endl;
+  cout << m_matrix << endl;
+
   return;
 }
 
